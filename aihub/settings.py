266f8b79eaf93a7403e4d42a853d508f341f6630
DS-Python-programmers-Spectrum <jiji_2445@spectrum.net.in>
2026-05-21 17:35:06 +0530

    first commit

--- aihub/settings.py
+++ aihub/settings.py
@@ -112,9 +112,7 @@ USE_I18N = True
 USE_TZ = True
 import os
 
-from decouple import config
 
-OPENAI_API_KEY = config('OPENAI_API_KEY')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
